5c606ab6e47e5b21bac02ea85364327159abf6c6
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-09-17 17:41:46 -0600

    Cambios en JWT

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/database');
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
-const userSchema = require('./src/models/user');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+
 
 dotenv.config(); // carga variables .env
 connectDB();     // conecta a MongoDB
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,12 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const JWT_SECRET = process.env.JWT_SECRET;
+const jwt = require('jsonwebtoken');
+//const JWT_SECRET = process.env.JWT_SECRET;
 // Rutas
 exports.createlogin= async (req, res) => {
-  const { email, password } = req.body;
+  const { name, password } = req.body;
   try {
-    const user = await User.findOne({username});
+    const user = await User.findOne({name});
     if (!user) {
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
@@ -14,27 +16,29 @@ exports.createlogin= async (req, res) => {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
 
-    const token = jwt.sign({userId: user._id}, JWT_SECRET, { expiresIn: '8h' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "8h",
+    });
     res.json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Error en el servidor', error });
+    res.status(500).json({ message: 'Error en el servidor', error : error.message });
   }
 };
 
 exports.register= async (req, res) => {
-  const { username, email, password } = req.body;
+  const { name, email, password } = req.body;
   try {
-    const existingUser = await User.findOne({username});
+    const existingUser = await User.findOne({name});
     if (existingUser) {
       return res.status(409).json({ message: 'El usuario ya existe' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: 'Usuario registrado exitosamente' });
   } catch (error) {
-    res.status(500).json({ message: 'Error en el servidor', error });
+    res.status(500).json({ message: 'Error en el servidor', error: error.message });
   }
 };
 
