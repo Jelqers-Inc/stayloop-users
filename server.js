8df92477c32cfbff5a77de5b4ba7ab7c9abd166e
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-10-02 09:08:01 -0600

    cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./src/config/database');
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
+
+
 
 
 dotenv.config(); // carga variables .env
@@ -12,6 +15,18 @@ connectDB();     // conecta a MongoDB
 const app = express();
 const PORT = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME || 'localhost';
+
+// Configuración de CORS
+const corsOptions = {
+  origin: '*', // Permite todas las origenes
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+  exposedHeaders: ['Content-Range', 'X-Content-Range'], // Headers expuestos
+  credentials: true, // Permite credenciales
+  maxAge: 86400 // Tiempo máximo de cache para los resultados de las solicitudes preflight
+};
+
+app.use(cors(corsOptions));
 
 
 app.use(express.json());
